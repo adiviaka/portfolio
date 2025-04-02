33f73f4ad07d7289cbaa89d874187fa4eeb74740
Adivia Khusnul Aisha <diva.adivia@gmail.com>
2025-04-02 23:40:09 +0700

    Fix React Hook dependency issues with useCallback

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { colors } from "../App";
-import React, { useCallback } from "react";
 
 function Experience() {
 	// State to track which experience type is active
